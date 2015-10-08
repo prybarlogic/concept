d25bcfbbb200424c5fcec21f1bf55deb5fef334b
SirBlastelot <stephensaelens@gmail.com>
2015-10-09 00:13:12 +0200

    Added src-steam project for testing SteamCMD

---
+++ src-steam/Commands/BaseCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace TerraPanel.Concept.Commands
+{
+    internal class BaseCommand
+    {
+        public ServerCommand ServerCommand { get; set; }
+
+        public string[] Args { get; set; }
+
+        public BaseCommand(ServerCommand serverCommand, string[] args)
+        {
+            this.ServerCommand = serverCommand;
+            this.Args = args;
+        }
+    }
+}
---
+++ src-steam/Example.cs
@@ -0,0 +1,50 @@
+﻿namespace TerraPanel.Concept
+{
+    using System;
+
+    class Example
+    {
+        public string SayHello(string myName)
+        {
+            if (myName == "Travis")
+            {
+                return "Hello Travis";
+            }
+            else
+            {
+                return "Hello SirB";
+            }
+        }
+        
+        public void MyOtherMethod()
+        {
+            RustPlayer player = new RustPlayer();
+            player.Username = "Travis";
+            player.SteamId = ulong.MaxValue;
+
+            string helloMessage = this.SayHello(player.Username);
+        }
+        
+        private object myObject = null;
+
+        public string name { get; set; }
+    }
+
+    public class RustPlayer
+    {
+        public string Username { get; set; }
+
+        public ulong SteamId { get; set; }
+
+        public RandomClass RandomClass { get; set; }
+    }
+
+    public class RandomClass
+    {
+        public string Name { get; set; }
+    }
+}
+
+
+// !=
+
